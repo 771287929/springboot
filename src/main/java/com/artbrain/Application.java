a9c50fe156aa2c22975aeb4a7b93233b8be5fd60
zhouwei <771287929@qq.com>
2016-08-19 18:48:08 +0800

    添加页面，使用Thymeleaf

--- src/main/java/com/artbrain/Application.java
+++ src/main/java/com/artbrain/Application.java
@@ -1,6 +1,7 @@
 package com.artbrain;
 
-import com.artbrain.dao.UserDetailsServiceDAO;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,8 +11,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import javax.sql.DataSource;
+import com.artbrain.dao.UserDetailsServiceDAO;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -36,6 +38,15 @@ public class Application extends WebMvcConfigurerAdapter {
     registry.addViewController("/").setViewName("home");
     registry.addViewController("/profile").setViewName("profile");
   }
+  
+ /* 默认再prefix：/templates/，suffix:.html 
+  * @Bean
+  public InternalResourceViewResolver viewResolver() {
+      InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+      resolver.setPrefix("/WEB-INF/view/");
+      resolver.setSuffix(".jsp");
+      return resolver;
+  }*/
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
--- src/main/java/com/artbrain/controller/UserController.java
+++ src/main/java/com/artbrain/controller/UserController.java
@@ -1,6 +1,14 @@
 package com.artbrain.controller;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.artbrain.dao.UserDetailsServiceDAO;
+import com.artbrain.entity.Product;
 import com.artbrain.entity.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +16,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
@@ -43,4 +53,42 @@ public class UserController {
     }
   }
 
+  @RequestMapping("/profile")
+  public String profile(Model model) throws ParseException{
+	  User user=new User();
+	  user.setUsername("周威");
+	  
+	  SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+	  Product p1=new Product(sdf.parse("2016-08-19"), new BigDecimal(2.5), "红色");
+	  Product p2=new Product(sdf.parse("2016-07-20"), new BigDecimal(2.8), "蓝色");
+	  Product p3=new Product(sdf.parse("2016-06-09"), new BigDecimal(3.5), "白色");
+	  Product p4=new Product(sdf.parse("2016-08-22"), new BigDecimal(2.0), "黑色");
+	  List<Product> productList=new ArrayList<Product>();
+	  productList.add(p1);
+	  productList.add(p2);
+	  productList.add(p3);
+	  productList.add(p4);
+	  
+	  model.addAttribute("user",user);
+	  model.addAttribute("productList",productList);
+    return "profile";
+  }
+  
+  
+  @RequestMapping("/profileJson")
+  @ResponseBody
+  public List<Product> findProduct() throws ParseException{
+	  
+	  SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+	  Product p1=new Product(sdf.parse("2016-08-19"), new BigDecimal(2.5), "红色");
+	  Product p2=new Product(sdf.parse("2016-07-20"), new BigDecimal(2.8), "蓝色");
+	  Product p3=new Product(sdf.parse("2016-06-09"), new BigDecimal(3.5), "白色");
+	  Product p4=new Product(sdf.parse("2016-08-22"), new BigDecimal(2.0), "黑色");
+	  List<Product> productList=new ArrayList<Product>();
+	  productList.add(p1);
+	  productList.add(p2);
+	  productList.add(p3);
+	  productList.add(p4);
+    return productList;
+  }
 }
